75647e55ccedd085dc32eacf0880196b7e516d46
Vyacheslav Kurbatov <Kurbatov-Slavard@yandex.ru>
2022-10-22 15:23:36 +0300

    add Example012 with "for"

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -18,10 +18,10 @@ void Method2(string msg)
 void Method21(string msg, int count)
 {
     int i = 0;
-    while (i<count)
+    while (i < count)
     {
         Console.WriteLine(msg);
-        i ++;
+        i++;
     }
 }
 // Method21(msg: "Текст", count: 4);
@@ -43,18 +43,47 @@ int year = Method3();
 // Вид 4
 
 
+// string Method4(int count, string text) // char тип данных, предназначенный для хранения одного символа
+// {
+//     int i = 0;
+//     string result = String.Empty; // переменная изначально будет пустой строкой
+
+//     while (i<count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, " asdf ");
+// Console.WriteLine(res);
+
 string Method4(int count, string text) // char тип данных, предназначенный для хранения одного символа
 {
-    int i = 0;
     string result = String.Empty; // переменная изначально будет пустой строкой
 
-    while (i<count)
+    for (int i = 0; i < count; i++)
     {
         result = result + text;
-        i++;
     }
     return result;
 }
 
 string res = Method4(10, " asdf ");
-Console.WriteLine(res);
+// Console.WriteLine(res); 
+
+
+
+
+//цикл в цикле
+// вывод таблицы умножения
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10 ; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
