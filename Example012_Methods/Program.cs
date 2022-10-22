16656cb65dda9da8e609411bf2367b457344afb7
Vyacheslav Kurbatov <Kurbatov-Slavard@yandex.ru>
2022-10-22 15:59:07 +0300

    Example012 Method with text

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -87,3 +87,60 @@ string res = Method4(10, " asdf ");
 //     }
 //     Console.WriteLine();
 // }
+
+
+
+//=====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы “к” заменить большими “К”,
+// а большие “С” маленькими “с”.
+
+// Ясна ли задача?
+
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwert"
+//             0123
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}"; // если текущий символ (text[i]) совпал с тем символом который мы хотим заменить (oldValue) то в результат (result) мы должны будем положить новое значение строки (newValue)
+        else result = result + $"{text[i]}"; // иначе (если не совпал) то положить текущий символ (тот что и был)
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+
+Console.WriteLine();
+
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+
+Console.WriteLine();
+
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
+
+
+
+
+
+
+
+
+
+
+
